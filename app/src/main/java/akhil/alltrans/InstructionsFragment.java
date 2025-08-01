a999337ae5ee775e304e0d316aecdb46a9bcad75
renzhidao <45433332+renzhidao@users.noreply.github.com>
2025-08-02 03:22:17 +0800

    更新 InstructionsFragment.java

--- app/src/main/java/akhil/alltrans/InstructionsFragment.java
+++ app/src/main/java/akhil/alltrans/InstructionsFragment.java
@@ -19,8 +19,6 @@
 
 package akhil.alltrans;
 
-import akhil.alltrans.BuildConfig;  // 添加这行 import
-
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -65,7 +63,7 @@ public class InstructionsFragment extends Fragment {
             }
         });
 
-        String versionName = BuildConfig.VERSION_NAME;
+        String versionName = "1.9.6";  // 直接使用版本号
         String system_language = Locale.getDefault().getLanguage();
         String url;
         String[] allowed_languages = {"hi", "ko", "af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "rw", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "or", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "tt", "te", "th", "tr", "tk", "uk", "ur", "ug", "uz", "vi", "cy", "xh", "yi", "yo", "zu"};
